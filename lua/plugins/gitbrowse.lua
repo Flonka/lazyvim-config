5f8ebe4800dfc99bebe68185738c34b77c99dd5d
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2025-09-17 22:08:01 +0300

    add onpremise bitbucket server gitbrowse configuration

---
+++ lua/plugins/gitbrowse.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      -- https://github.com/folke/snacks.nvim/blob/main/docs/gitbrowse.md
+      -- https://github.com/folke/snacks.nvim/blob/main/lua/snacks/gitbrowse.lua
+      gitbrowse = {
+        what = "permalink",
+        remote_patterns = {
+          -- Onpremise Bitbucket Server SSH pattern
+          {
+            "ssh://git@bitbucket.crosskey.fi:7999/(.*)/(.*).git",
+            "https://bitbucket.crosskey.fi/projects/%1/repos/%2/browse",
+          },
+        },
+        url_patterns = {
+          ["bitbucket.crosskey.fi"] = {
+            branch = "/?at=refs/heads/{branch}",
+            file = "/{file}#{line_start}-{line_end}",
+            permalink = "/{file}?at={commit}#{line_start}-{line_end}",
+            commit = "/commits/{commit}",
+          },
+        },
+      },
+    },
+  },
+}
