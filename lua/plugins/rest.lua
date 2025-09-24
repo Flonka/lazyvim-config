35cf3345bd12d2b80b5fa05ed413c6a2148d46e2
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2025-09-24 13:09:23 +0300

    use latest kulala, add custom dynamic variable

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,8 +1,16 @@
 return {
   {
     "mistweaverco/kulala.nvim",
+    -- NOTE: using latest n greatest
+    branch = "develop",
     opts = {
       additional_curl_options = { "--insecure" },
+      custom_dynamic_variables = {
+        ["$random.email"] = function()
+          local random = math.random(1000, 9999)
+          return "user" .. random .. "@example.com"
+        end,
+      },
     },
   },
 }
