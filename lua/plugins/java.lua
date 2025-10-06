21852f873e1bb9942de61dc20b3dfb6c41e89e4e
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2025-10-06 15:52:14 +0300

    Updates and jdtls config change

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -3,13 +3,7 @@ return {
     "mfussenegger/nvim-jdtls",
     opts = {
       root_dir = function(path)
-        -- NOTE: Altered due to needing to support multiprojects, which have many build.gradle files.
-        local custom = vim.fs.root(path, { "gradlew", "mvnw" })
-        if custom then
-          return custom
-        end
-        -- fallback to default configuration
-        return vim.fs.root(path, vim.lsp.config.jdtls.root_markers)
+        return vim.fs.root(0, { "gradlew", "mvnw" })
       end,
     },
   },
