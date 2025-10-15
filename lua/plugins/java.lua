d20d1ec6d9c909fb45d1ad00cf623cd969ee0866
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2025-10-15 15:48:00 +0300

    add runtimes

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -7,9 +7,33 @@ return {
       end,
 
       cmd = (function()
-        -- Use vim.fn.expand to expand ~ in JAVA_HOME path
+        -- set JAVA_HOME for the JDTLS to run JDK21
         vim.env.JAVA_HOME = "/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home"
       end)(),
+
+      settings = {
+        java = {
+          configuration = {
+            -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+            -- And search for `interface RuntimeOption`
+            -- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
+            runtimes = {
+              {
+                name = "JavaSE-1.8",
+                path = "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home",
+              },
+              {
+                name = "JavaSE-17",
+                path = "/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home",
+              },
+              {
+                name = "JavaSE-21",
+                path = "/Library/Java/JavaVirtualMachines/temurin-20.jdk/Contents/Home",
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
