2acdb48a8ef78b83cd5f7ab53428d3fbd6cbc721
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2025-10-15 15:11:22 +0300

    jdtls needs jdk21

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -5,6 +5,11 @@ return {
       root_dir = function(path)
         return vim.fs.root(0, { "gradlew", "mvnw" })
       end,
+
+      cmd = (function()
+        -- Use vim.fn.expand to expand ~ in JAVA_HOME path
+        vim.env.JAVA_HOME = "/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home"
+      end)(),
     },
   },
 }
